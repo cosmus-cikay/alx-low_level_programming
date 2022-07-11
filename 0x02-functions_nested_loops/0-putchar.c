3b717bf5e983eb0858d46699585a1b930ac7c8e0
cosmus-cikay <cosmuskaggwa@gmail.com>
2022-07-11 11:01:48 -0700

    ammend

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * main - function to print
- * Return: 0
+ * main - main block
+ * Description: print _putchar followed on a new line
+ * Return: always 0
  */
 int main(void)
 {
-	
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -18,5 +18,4 @@ int main(void)
 	_putchar('\n');
 
 	return (0);
-
 }
--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,24 +1,27 @@
 #include "main.h"
 /**
- * print_sign - function to check for a sign of a number
- * @n:  is the int that will use for the argument of the function
- * Return: 0
+ * print_sign - entry point
+ * @n : number to check
+ * Return:0 or 1
  */
 int print_sign(int n)
 {
+	int numz;
+
 	if (n > 0)
 	{
+		numz = 1;
 		_putchar('+');
-		return (1);
 	}
-	else if (n < 0)
+	else if (n == 0)
 	{
-		_putchar('-');
-		return (-1);
+		numz = 0;
+		_putchar('0');
 	}
 	else
 	{
-		_putchar('0');
-		return (0);
+		numz = -1;
+		_putchar('-');
 	}
+	return (numz);
 }
